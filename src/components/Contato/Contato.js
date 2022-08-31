e8d49d69c5c5fc9ce7ac3d4321c8d0f38371eb4b
Gabriel Duarte <gabrield.developer@gmail.com>
2022-08-31 00:17:04 -0300

    chore(App): creates animation for component entrance

--- src/components/Contato/Contato.js
+++ src/components/Contato/Contato.js
@@ -4,7 +4,7 @@ import foto from '../../img/contato.jpg';
 
 const Contato = () => {
   return (
-    <section className={styles.contato}>
+    <section className={`${styles.contato} animeLeft`}>
       <img src={foto} alt="Máquina de escrever" />
       <div>
         <h1>Entre em contato</h1>
